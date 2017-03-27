fe8ed5907afcbe0dcc39840c3a8d5fab95b37813
Joni <cph-js226@cphbusiness.dk>
2017-03-27 15:05:56 +0200

    errors blabla

--- depressed.py
+++ depressed.py
@@ -6,8 +6,14 @@ import operator
 
 print("Reading csv from URL")
 
+#Event.Id, Investigation.Type, Accident.Number, Event.Date, Location, Country, Latitude, Longitude, Airport.Code, Airport.Name, Injury.Severity, Aircraft.Damage, Aircraft.Category, Registration.Number, Make, Model, Amateur.Built, Number.of.Engines, Engine.Type, FAR.Description, Schedule, Purpose.of.Flight, Air.Carrier, Total.Fatal.Injuries, Total.Serious.Injuries, Total.Minor.Injuries, Total.Uninjured, Weather.Condition, Broad.Phase.of.Flight, Report.Status, Publication.Date
+
+
+
+
 
 df = pd.read_csv("AviationDataset.csv", encoding ='latin1').fillna(0)
+print(df[:0])
 #df = pd.read_csv("https://raw.githubusercontent.com/edipetres/Depressed_Year/master/Dataset_Assignment/AviationDataset.csv").fillna(0)
 
 print("file loaded")
@@ -36,7 +42,22 @@ result_dict = dict(sorted(injury_dict.items(), key=operator.itemgetter(1), rever
 # Plotting, fucking plotting
 x, y = zip(*sorted(result_dict.items()))
 data = pd.Series(y, x)
-bar2 = Bar(data, ylabel="hej", title="Top 5 States Of Aviation Injuries", plot_width=400, color="origin")
+bar2 = Bar(data, title="Top 5 States Of Aviation Injuries", plot_width=400)
 
 
 show(bar2)
+
+
+## 3
+# df['Airport.Name']
+#
+#
+# aircraftInjuries_dict = {}
+# for state in us_states:
+#     aircraftInjuries_dict.update({ state : sum(df[df['Airport.Name']][cat]) for cat in ['Total.Fatal.Injuries', 'Total.Serious.Injuries', 'Total.Minor.Injuries']} )
+# result_dict = dict(sorted(aircraftInjuries_dict.items(), key=operator.itemgetter(1), reverse=True)[:5])
+#
+# # Plotting, fucking plotting
+# x, y = zip(*sorted(result_dict.items()))
+# data = pd.Series(y, x)
+# bar2 = Bar(data, ylabel="hej", title="Top 5 States Of Aviation Injuries", plot_width=400, color="origin")
