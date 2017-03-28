dc1c713d575ee1d81e592409d3db6f0a5600d914
szEIgo <cph-js226@cphbusiness.dk>
2017-03-29 00:03:34 +0200

    changes were made

--- depressed.py
+++ depressed.py
@@ -1,51 +1,53 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from bokeh.charts import Donut, Bar, show, vplot, Line
+from bokeh.charts import Donut, Bar, show, vplot, Line, output_file
 import operator
+from bokeh.charts.attributes import cat, color
+from bokeh.charts.operations import blend
+from bokeh.charts.utils import df_from_json
+from bokeh.sampledata.olympics2014 import data
 from tqdm import tqdm
+#import msvcrt as m
+
 
 print("Reading csv from URL")
 
-#Event.Id, Investigation.Type, Accident.Number, Event.Date, Location, Country, Latitude, Longitude, Airport.Code, Airport.Name, Injury.Severity, Aircraft.Damage, Aircraft.Category, Registration.Number, Make, Model, Amateur.Built, Number.of.Engines, Engine.Type, FAR.Description, Schedule, Purpose.of.Flight, Air.Carrier, Total.Fatal.Injuries, Total.Serious.Injuries, Total.Minor.Injuries, Total.Uninjured, Weather.Condition, Broad.Phase.of.Flight, Report.Status, Publication.Date
 
 df = pd.read_csv("AviationDataset.csv", encoding ='latin1').fillna(0)
-print(df[:0])
-# #df = pd.read_csv("https://raw.githubusercontent.com/edipetres/Depressed_Year/master/Dataset_Assignment/AviationDataset.csv").fillna(0)
-#
-# print("file loaded")
-#
+print("file loaded")
+
 # ## 1
-# fatality_dict = {}
-# for phase in df['Broad.Phase.of.Flight'].unique():
+#fatality_dict = {}
+#for phase in df['Broad.Phase.of.Flight'].unique():
 #     if phase != 0:
 #         fatality_dict.update({ phase : sum(df[df['Broad.Phase.of.Flight'] == phase]['Total.Fatal.Injuries']) / len(df[df['Broad.Phase.of.Flight'] == phase]) * 100})
-#
-# ## Plotting Some Stuff
-# x, y = zip(*sorted(fatality_dict.items()))
-# data = pd.Series(y, x)
-# pie_chart = Donut(data, plot_width=800, plot_height=800)
-# # show(pie_chart)
-#
-# ## 2
-# df['Location'] = df['Location'].apply(lambda word: str(word)[-2:])
-# us_states = df.loc[df['Country'] == 'United States']['Location'].unique()
-#
-# injury_dict = {}
-# for state in us_states:
-#     injury_dict.update({ state : sum(df[df['Location'] == state][cat]) for cat in ['Total.Fatal.Injuries', 'Total.Serious.Injuries', 'Total.Minor.Injuries']} )
-# result_dict = dict(sorted(injury_dict.items(), key=operator.itemgetter(1), reverse=True)[:5])
-#
-# # Plotting, fucking plotting
-# x, y = zip(*sorted(result_dict.items()))
-# data = pd.Series(y, x)
-# bar2 = Bar(data, title="Top 5 States Of Aviation Injuries", plot_width=400)
-#
-# show(bar2)
+#x, y = zip(*sorted(fatality_dict.items()))
+#data = pd.Series(y, x)
+#pie_chart = Donut(data, plot_width=800, plot_height=800)
+#def wait():
+#    m.getch()
+#show(pie_chart)
+
+# # 2
+#df['Location'] = df['Location'].apply(lambda word: str(word)[-2:])
+#us_states = df.loc[df['Country'] == 'United States']['Location'].unique()
+
+#injury_dict = {}
+#for state in us_states:
+#    injury_dict.update({ state : sum(df[df['Location'] == state][cat]) for cat in ['Total.Fatal.Injuries', 'Total.Serious.Injuries', 'Total.Minor.Injuries']} )
+#result_dict = dict(sorted(injury_dict.items(), key=operator.itemgetter(1), reverse=True)[:5])
+#x, y = zip(*sorted(result_dict.items()))
+#data = pd.Series(y, x)
+#bar2 = Bar(data, title="Top 5 States Of Aviation Injuries", plot_width=400)
+
+#show(bar2)
 
 
 ## 3
-# minor_dict = serious_dict = fatal_dict  = dict.fromkeys(df['Model'].unique(), 0)
+# minor_dict = dict.fromkeys(df['Model'].unique(), 0)
+#serious_dict = dict.fromkeys(df['Model'].unique(), 0)
+#fatal_dict  = dict.fromkeys(df['Model'].unique(), 0)
 #
 # for row in df.itertuples():
 #     minor, serious, fatal = [int(row[cat]) for cat in [24, 25, 26]]
@@ -101,7 +103,8 @@ print(df[:0])
 # show(chart)
 
 # 5
-year_fatal = year_alive = dict.fromkeys(range(1993, 2018), 0)
+year_fatal = dict.fromkeys(range(1993, 2018), 0)
+year_alive = dict.fromkeys(range(1993, 2018), 0)
 for idx, row in tqdm(df[['Event.Date', 'Aircraft.Damage', 'Total.Fatal.Injuries', 'Total.Minor.Injuries', 'Total.Serious.Injuries', 'Total.Uninjured']].iterrows()):
     cats = ['Destroyed', 'Substantial']
     year = int(row['Event.Date'][:4])
@@ -110,12 +113,27 @@ for idx, row in tqdm(df[['Event.Date', 'Aircraft.Damage', 'Total.Fatal.Injuries'
         alive = sum(row[cat] for cat in ['Total.Serious.Injuries', 'Total.Minor.Injuries', 'Total.Uninjured'])
         year_fatal.update({year : year_fatal.get(year) + fatal})
         year_alive.update({year : year_alive.get(year) + alive})
-
 fx, fy = zip(*sorted(year_fatal.items()))
-fatal_data = pd.Series(fy, fx)
+fatal_data = pd.Series(fy, fx, name="fatal")
+#print(fatal_data.head(5))
 
 ax, ay = zip(*sorted(year_alive.items()))
-alive_data = pd.Series(ay, ax)
+alive_data = pd.Series(ay, ax, name="alive")
+#print(alive_data.head(5))
 
-chart = Bar(fatal_data, title="Fatal Injury Distribution", legend="top_right", ylabel='Number of Fatalities', xlabel='Year')
+data = pd.concat([alive_data, fatal_data], axis=1)
+
+#http://datascience.stackexchange.com/questions/10322/how-to-plot-multiple-variables-with-pandas-and-bokeh
+
+#chart = Bar(data,title="Fatal Injury Distribution", legend="top_right", ylabel='Number of Fatalities', xlabel='Year')
+#chart = Bar(data,
+#          values=blend('fatal', 'alive', name='persons', labels_name='person'),
+#          label=cat(columns='year', sort=False),
+#          stack=cat(columns='person', sort=False),
+#          color=color(columns='person', 
+#          palette=['SaddleBrown', 'Silver'],
+#          sort=False))
+
+chart = Bar(data, stacked=True)
 show(chart)
+
